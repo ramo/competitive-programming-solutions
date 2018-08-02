4efd286e4d8aa1df6d7da27ad981b103675e4170
ramo <ramo.phoenix7@gmail.com>
2018-08-03 01:07:42 +0530

    Solved a problem from search section medium problem

--- js/hackerearth/normal_type.js
+++ js/hackerearth/normal_type.js
@@ -3,11 +3,45 @@
  */
 "use strict"
 
+    function get_dist_ints(arr) {
+        var hash = {};
+        var count = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (!hash[arr[i]]) {
+                ++count;
+                hash[arr[i]] = true;
+            }
+        }
+        return count;
+    }
+     
 function main(args) {
-    var n = Number(args.shift());
-    var arr = args[0].split(" ").map(Number);
-    var uSet = new Set(arr);
-    console.log(Math.pow(2, (n-uSet.size)));
+        var arr = args[1].split(" ");
+        var n = arr.length;
+        var dist_ints = get_dist_ints(arr);
+        var count = 0;
+        var i = 0;
+        var j = -1;
+        var c_ints = 0;
+        var hash = {};
+     
+        while (j < n) {
+            if (dist_ints === c_ints) {
+                count += n-j;
+                hash[arr[i]] -= 1;
+                if (hash[arr[i]] === 0) {
+                    --c_ints;
+                }
+                i++;
+            } else {
+                j++;
+                if (!hash[arr[j]])
+                    ++c_ints;
+                hash[arr[j]] = (hash[arr[j]] || 0) + 1;
+            }
+        }
+     
+        console.log(count);
 }
 
 process.stdin.resume();
