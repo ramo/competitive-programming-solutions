c7b0529c1c9791b76634e3aab759ea1712b081eb
ramo <ramo.phoenix7@gmail.com>
2018-11-26 19:45:35 +0530

    Solved some hackerrank problems from tress and stack and queues sections

---
+++ java8/hackerrank/interview_preparation_kit/trees/CheckBST.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * https://www.hackerrank.com/challenges/ctci-is-binary-search-tree/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=trees
+ */
+
+/* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.  
+
+The Node class is defined as follows:
+    class Node {
+        int data;
+        Node left;
+        Node right;
+     }
+*/
+    boolean checkBST(Node root) {
+        return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    
+    boolean checkBST(Node root, int min, int max) {
+        if (root == null) {
+            return true;
+        }
+        
+        if (root.data < min || root.data > max) {
+            return false;
+        }
+            
+        return checkBST(root.left, min, root.data-1) && 
+            checkBST(root.right, root.data+1, max);
+    }
